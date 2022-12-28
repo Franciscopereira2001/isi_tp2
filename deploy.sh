67dde9c404eabdeddc6b9ae856ff5701df3a9a95
Francisco <franciscorafeldpereira@gmail.com>
2022-12-28 06:46:09 +0000

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,19 @@
 set -ev
 source .env
+gcloud config set project smartenergy-backend
+
+gcloud services enable \
+  run.googleapis.com \
+  sql-component.googleapis.com \
+  sqladmin.googleapis.com \
+  compute.googleapis.com \
+  cloudbuild.googleapis.com \
+  secretmanager.googleapis.com \
+  artifactregistry.googleapis.com
+
 
 gcloud builds submit --pack image=gcr.io/${PROJECT_ID}/myimage --project ${PROJECT_ID}
-gcloud builds submit --config migrate.yaml --project ${PROJECT_ID}
+gcloud builds submit --config migrate.yaml --project ${PROJECT_ID} --substitutions _REGION=$REGION
 gcloud run deploy django-cloudrun \
   --platform managed \
   --region $REGION \
